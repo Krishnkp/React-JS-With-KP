9a6552db5ad4586f5eb321919b9cd8a45d566d50
Krishnkp <krishnp.tla@gmail.com>
2023-09-18 16:36:50 +0530

    first first introduce yourself project-2

---
+++ basicreact/src/App.js
@@ -0,0 +1,36 @@
+import logo from './kp.svg';
+import './App.css';
+
+function App() {
+  return (
+
+    <html>
+      <head>
+          <title>Krishn Pethani - Introduction</title>
+      </head>
+
+      <div className="App">
+          <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />  
+          </header>
+      </div>
+
+      <body>
+          <h1>Hello, I'm Krishn Pethani</h1>
+
+          <p>I'm delighted to introduce myself as a junior Magento 2 developer currently working at The Look Agency. My professional journey in the world of web development has been an exciting one so far, and I'm eager to continue learning and growing in this field.</p>
+          
+          <p>At The Look Agency, my role involves working on Magento 2 projects, where I contribute to the development of e-commerce solutions that help businesses thrive in the digital landscape. It's a dynamic and challenging environment that allows me to apply my technical skills and problem-solving abilities.</p>
+          
+          <p>I am based in Upaleta, where I have the opportunity to collaborate with a talented team of professionals who share my passion for technology. Together, we aim to create innovative and user-friendly online experiences for our clients.</p>
+          
+          <p>Outside of work, I have a keen interest in reading books. Books have always been my source of inspiration and knowledge, and I believe they have the power to broaden our horizons and spark creativity. In addition to reading, I enjoy exploring new places, discovering different cultures, and embracing the beauty of diversity.</p>
+          
+          <p>I am dedicated to continuous self-improvement and always strive to stay up-to-date with the latest developments in web development and technology. I look forward to connecting with fellow professionals, sharing experiences, and contributing to the ever-evolving world of web development.</p>
+      </body>
+    </html>
+
+  );
+}
+
+export default App;
