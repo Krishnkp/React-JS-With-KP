782ba6582bdb3b700c46a9a8d8f5e3b8bef71c67
Krishnkp <krishnp.tla@gmail.com>
2023-09-18 16:58:38 +0530

    react-js with JSX project

---
+++ jsxreact/src/App.js
@@ -0,0 +1,21 @@
+import './App.css';
+let kano = "Krishn";
+function App() {
+  return (
+          <>
+            <nav>
+              <li>KP1</li>
+              <li>KP2</li>
+              <li>KP3</li>
+            </nav>
+            <div className="kano">
+              <h1>My Name is {kano}</h1>
+              <p>
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati aperiam molestiae tenetur veritatis sapiente maiores rerum iure, repellendus, laborum facilis doloremque, fugit illum eos eveniet quis voluptatem qui ab velit?
+              </p>
+            </div>
+          </>
+  );
+}
+
+export default App;
